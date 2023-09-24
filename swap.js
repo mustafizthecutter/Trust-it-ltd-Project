c780c651cd90a49d5974513483c9270505ab4f68
Mustafizur Rahman <mustafizur232@gmail.com>
2023-09-24 23:57:31 +0600

    destructuring done

--- swap.js
+++ swap.js
@@ -1,7 +1,13 @@
+// swapping done first approach
 let num1 = 6;
 let num2 = 8;
 
-const temp = num1;
-num1 = num2;
-num2 = temp;
+// const temp = num1;
+// num1 = num2;
+// num2 = temp;
+// console.log(num1, num2);
+
+// swapping done second approach: Destructuring****
+
+[num1, num2] = [num2, num1];
 console.log(num1, num2);
