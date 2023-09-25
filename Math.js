5f972e7298dbd2f54dbc9cc4376c069d4b4aa5f7
Mustafizur Rahman <mustafizur232@gmail.com>
2023-09-25 16:20:53 +0600

    Math Function Added

---
+++ Math.js
@@ -0,0 +1,24 @@
+const num1 = 20;
+const num2 = 50;
+
+const gap = Math.abs(num1 - num2);
+// console.log(gap);
+if (gap < 10) {
+    // console.log('Its Okay');
+}
+else {
+    // console.log('Its not Okay');
+}
+
+const num3 = Math.round(4.2398);
+// console.log(num3);
+const num4 = Math.floor(2.23);
+// console.log(num4);
+
+const num5 = Math.random() * 10;
+// console.log(num5);
+
+for (let i = 0; i < 20; i++) {
+    const newNumber = Math.round(Math.random() * 6);
+    // console.log(newNumber);
+}
