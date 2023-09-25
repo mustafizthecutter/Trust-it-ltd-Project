cd36f98502be4ad0cae3d4dca3b3b1ecf4040510
Mustafizur Rahman <mustafizur232@gmail.com>
2023-09-25 18:38:20 +0600

    all examples are done

---
+++ Max.js
@@ -0,0 +1,52 @@
+// using if-else-----------------------------
+
+
+// const abir = 26;
+// const niaz = 95;
+// const tazed = 43;
+
+// if ((abir > niaz) && (abir > tazed)) {
+//     console.log('abir gets the maark');
+// }
+// else if ((niaz > abir) && (niaz > tazed)) {
+//     console.log('niaz gets the mark');
+// }
+// else {
+//     console.log('tazed gets the mark');
+// }
+
+// using Max-Min function-------------------------
+
+// const getMax = Math.max(abir, niaz, tazed);
+// console.log(getMax);
+
+// using function of Max-Min Function--------------
+
+// function getMax(abir, niaz, tazed) {
+//     const numbers = Math.max(abir, niaz, tazed);
+//     return numbers;
+// }
+// const abir = 26;
+// const niaz = 95;
+// const tazed = 43;
+// const finalNumbers = getMax(abir, niaz, tazed);
+// console.log(finalNumbers);
+
+// using if-else function inside of a function-------------
+
+// function getMax(abir, niaz, tazed) {
+//     if (abir > niaz && abir > tazed) {
+//         return 'abir gets the mark';
+//     }
+//     else if (niaz > abir && niaz > tazed) {
+//         return 'niaz gets the mark';
+//     }
+//     else {
+//         return 'tazed gets the mark';
+//     }
+// }
+// const abir = 26;
+// const niaz = 56;
+// const tazed = 93;
+// const getFinalNumbers = getMax(abir, niaz, tazed);
+// console.log(getFinalNumbers);
---
+++ Min.js
@@ -0,0 +1,49 @@
+// using if-else---------------------------------
+
+// if ((abir < niaz) && (abir < tazed)) {
+//     console.log('abir gets the mark');
+// }
+// else if ((niaz < abir) && (niaz < tazed)) {
+//     console.log('niaz gets the mark');
+// }
+// else {
+//     console.log('tazed gets the mark');
+// }
+
+// using max-min function------------------------
+
+// const getMin = Math.min(abir, niaz, tazed);
+// console.log(getMin);
+
+
+// using function of Max-Min Function--------------------------------
+
+// function getMin(abir, niaz, tazed) {
+//     const numbers = Math.min(abir, niaz, tazed);
+//     return numbers;
+// }
+// const abir = 26;
+// const niaz = 95;
+// const tazed = 43;
+// const finalNumbers = getMin(abir, niaz, tazed);
+// console.log(finalNumbers);
+
+
+// using if-else function inside of a function-------------
+
+// function getMin(abir, niaz, tazed) {
+//     if (abir < niaz && abir < tazed) {
+//         return 'abir gets the mark';
+//     }
+//     else if (niaz < abir && niaz < tazed) {
+//         return 'niaz gets the mark';
+//     }
+//     else {
+//         return 'tazed gets the mark';
+//     }
+// }
+// const abir = 26;
+// const niaz = 56;
+// const tazed = 93;
+// const getFinalNumbers = getMin(abir, niaz, tazed);
+// console.log(getFinalNumbers);
