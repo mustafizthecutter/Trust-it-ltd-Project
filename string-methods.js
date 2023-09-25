bb5f94f2f1eed71a2c61ab75ac6fee2712238656
Mustafizur Rahman <mustafizur232@gmail.com>
2023-09-25 14:23:05 +0600

    addeds again

---
+++ string-methods.js
@@ -0,0 +1,9 @@
+const name = "Mustafizur Rahman is a good boy. He is a Female Person. He Reads in class seven";
+const piece = name.split(' ');
+const dots = name.split('.');
+const single = name.split('');
+const cut = name.slice(5, 9);
+const subSet = name.substring(9, 15);
+// console.log(dots);
+const newPara = dots.join(':');
+console.log(newPara);
