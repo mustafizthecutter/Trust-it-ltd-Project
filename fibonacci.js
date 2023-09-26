c06b5fcb61c3d1ec5840a7642e6b459355d6a8e0
Mustafizur Rahman <mustafizur232@gmail.com>
2023-09-26 22:22:55 +0600

    more js concepts

--- fibonacci.js
+++ fibonacci.js
@@ -1,6 +1,21 @@
+// example of Fibonacci Series:
 const fibo = [0, 1];
 for (i = 2; i <= 12; i++) {
     fibo[i] = fibo[i - 1] + fibo[i - 2];
 
 }
-console.log(fibo);
+// console.log(fibo);
+
+
+// More Js Concepts:
+const friends = [13, 85, 26, 46, 17];
+console.log(Array.isArray(friends));
+console.log(friends.includes(50));
+if (friends.indexOf(67) !== -1) {
+    console.log('yes');
+}
+else {
+    console.log('no');
+}
+const newFriends = [64, 20, 83, 67];
+console.log(friends.concat(newFriends));
--- reverse-string.js
+++ reverse-string.js
@@ -1,12 +1,12 @@
-function myString(text) {
-    let reversed = '';
-    for (i = text.length - 1; i >= 0; i--) {
-        const element = text[i];
-        reversed = reversed + element;
-        // console.log(element);
-    }
-    return reversed;
-}
-const text = 'Mustafiz is a good boy.'
-const result = myString(text);
-console.log(result);
+// function myString(text) {
+//     let reversed = '';
+//     for (i = text.length - 1; i >= 0; i--) {
+//         const element = text[i];
+//         reversed = reversed + element;
+//         // console.log(element);
+//     }
+//     return reversed;
+// }
+// const text = 'Mustafiz is a good boy.'
+// const result = myString(text);
+// console.log(result);
