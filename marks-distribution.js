bab89874649013cdd8524744810d729b36271ce0
Mustafizur Rahman <mustafizur232@gmail.com>
2023-09-28 03:31:22 +0600

    concat array done

--- marks-distribution.js
+++ marks-distribution.js
@@ -26,3 +26,13 @@ function totalDistributionRate(number) {
 }
 const discountValueTotal = totalDistributionRate(120);
 console.log(discountValueTotal);
+
+
+
+// adding two using concat method--------------
+
+
+// const num1 = [12, 63, 19, 78, 23];
+// const num2 = [23, 69, 54, 58, 27];
+// const num3 = num1.concat(num2);
+// console.log(num3);
